75bec1449af1a1c833d8e15be5e9727cd716dc78
Dmitrii Kashirin <bambuchastudent@gmail.com>
2026-08-13 17:03:36 +0530

    feat: cache player shell for PWA

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'winampmusic-shell-v1';
+const SHELL = [
+  './',
+  './index.html',
+  './styles.css',
+  './youtube-player.css',
+  './app.js',
+  './youtube-import.js',
+  './manifest.webmanifest',
+  './icon.svg',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin || event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        if (response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        }
+        return response;
+      });
+    })
+  );
+});
