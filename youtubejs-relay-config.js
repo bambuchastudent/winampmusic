5bfcb824a7d19fc499ff9ba3c7d1281d39756171
Dmitrii Kashirin <bambuchastudent@gmail.com>
2026-09-19 11:52:34 +0200

    feat: add inert YouTube.js relay config

---
+++ youtubejs-relay-config.js
@@ -0,0 +1,5 @@
+(() => {
+  'use strict';
+  // Production delivery may replace this inert value with the dedicated relay origin.
+  window.AMPULA_YOUTUBEJS_RELAY = window.AMPULA_YOUTUBEJS_RELAY || '';
+})();
